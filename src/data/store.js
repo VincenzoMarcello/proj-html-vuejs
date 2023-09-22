4d23a7b0b31331ed3eca7cf790ed7adb1fdafb1d
VincenzoMarcello <vincenzo.marcello.92@gmail.com>
2023-09-22 16:37:47 +0200

    fix: various fix

--- src/data/store.js
+++ src/data/store.js
@@ -4,7 +4,4 @@ import { reactive } from "vue";
 
 // # mettiamo lo store in una costante che esporteremo dove ci serve
 // # e invochiamo appunto il metdo che renderà reattivo l'oggetto
-export const store = reactive({
-  movies: [],
-  series: [],
-});
+export const store = reactive({});
